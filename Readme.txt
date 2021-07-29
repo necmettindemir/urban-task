1801fc16db06089708767f948c6219d357ab6827
necmetti <necmettindemiryahoo.com>
2021-07-29 11:33:28 +0100

    first commit

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+The aim of this work is to create a method as REST API.
+This method will take a text as part of an address.
+Later, this text will be sent to google-like address provider.
+The returned value will be used to search in local file or data.
+The searched value will be returned in the desired format.
+
+Typescript is used.
--- env.config.ts
+++ env.config.ts
@@ -6,6 +6,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+//require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+
+/**
+ dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+});
+ */
+
 export const ENV_API_URL = process.env.ENV_API_URL;
 export const ENV_API_KEY = process.env.ENV_API_KEY;
 export const ENV_PORT = process.env.ENV_PORT;
